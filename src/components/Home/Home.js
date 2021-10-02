f1a520e9ac5fbc3f60394c78fe3d68795b8e3d66
Swastik <swastik.sonkusare18@gmail.com>
2021-10-02 11:41:24 +0530

    remove axios

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,33 +6,10 @@ import { socialMediaIcons } from "../../assets/data";
 import "./Home.scss";
 import Button from "../Button/Button";
 
-import axios from "axios";
-
 const Home = () => {
   const noMargin = true;
   const name = ["S", "w", "a", "s", "t", "i", "k"];
   const surname = ["S", "o", "n", "k", "u", "s", "a", "r", "e"];
-
-  // fetch("/?json=true").then(async (response) => {
-  //   try {
-  //     const data = await response;
-  //     console.log("response data?", data);
-  //   } catch (error) {
-  //     console.log("Error happened here!");
-  //     console.error(error);
-  //   }
-  // });
-
-  // const fetch = async () => {
-  //   const { data } = await axios.get("/index.html");
-
-  //   const a = JSON.stringify(data);
-  //   console.log(a);
-  //   const b = JSON.parse(a);
-  //   console.log(b);
-  // };
-
-  // fetch();
 
   return (
     <main id="home" className="home">
