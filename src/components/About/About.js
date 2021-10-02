d1187a1f63362654d5df29034755d7ae09c9423f
Swastik <swastik.sonkusare18@gmail.com>
2021-10-02 11:38:01 +0530

    made changes in about screen

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,16 +9,15 @@ const About = () => {
       <h1 className="home__heading">
         About <span className="home__heading--secondary">Me</span>
       </h1>
-      {/* <h2 className="home__sub-heading">Web Developer</h2> */}
       <div className="about__info">
         <div className="menu__portrait"></div>
         <div className="about__bio">
           <h3 className="home__heading--secondary">Bio</h3>
           <p>
-            I'm Full Stack Web Developer but more importantly I'm passionate
+            I am a Full Stack Web Developer but more importantly I am passionate
             about technologies.
             <br />I am always eager to learn new skills and grow my technical
-            skill set in a team-based atomosphere.
+            skill-set in a team-based atmosphere.
           </p>
           <Button />
         </div>
